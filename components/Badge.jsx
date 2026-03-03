2293065a95dedbd2bb796268b294d5f6e120f598
Taviquenson <tavo2070@gmail.com>
2026-03-03 15:03:05 -0800

    add badges

--- components/Badge.jsx
+++ components/Badge.jsx
@@ -1,7 +1,16 @@
 import clsx from 'clsx';
 
 export default function Badge({children, color, shape}) {
-    const styles = clsx('badge', color==="red" && 'badge-red', shape==="pill" && 'badge-pill')
+    const styles = clsx('badge',
+        color==="gray" && 'gray',
+        color==="red" && 'red',
+        color==="yellow" && 'yellow',
+        color==="green" && 'green',
+        color==="blue" && 'blue',
+        color==="indigo" && 'indigo',
+        color==="purple" && 'purple',
+        color==="pink" && 'pink',
+        shape==="pill" && 'badge-pill')
 
     return (
         <div class={styles}>{children}</div>
--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,38 @@ function App() {
 
   return (
     <>
-      <h1>Start!</h1>
-      <Badge color="red" shape="pill">My first badge</Badge>
+      <h1>React Library Components</h1>
+      <section class="badges">
+      <h2 class="badges-section-title">Badges</h2>
+      <div class="badges-table">
+        <div class="badges-shape-square">
+          <h3>Square</h3>
+          <div class="badges-container">
+            <Badge color="gray">Badge</Badge>
+            <Badge color="red">Badge</Badge>
+            <Badge color="yellow">Badge</Badge>
+            <Badge color="green">Badge</Badge>
+            <Badge color="blue">Badge</Badge>
+            <Badge color="indigo">Badge</Badge>
+            <Badge color="purple">Badge</Badge>
+            <Badge color="pink">Badge</Badge>
+          </div>
+        </div>
+        <div class="badges-shape-pill">
+          <h3>Pill</h3>
+          <div class="badges-container">
+            <Badge color="gray" shape="pill">Badge</Badge>
+            <Badge color="red" shape="pill">Badge</Badge>
+            <Badge color="yellow" shape="pill">Badge</Badge>
+            <Badge color="green" shape="pill">Badge</Badge>
+            <Badge color="blue" shape="pill">Badge</Badge>
+            <Badge color="indigo" shape="pill">Badge</Badge>
+            <Badge color="purple" shape="pill">Badge</Badge>
+            <Badge color="pink" shape="pill">Badge</Badge>
+          </div>
+        </div>
+      </div>
+      </section>
     </>
   )
 }
