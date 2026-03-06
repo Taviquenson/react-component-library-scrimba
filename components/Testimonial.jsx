bd8adca2935bd61ae4a30d2c7840daa10f79b3fd
Taviquenson <tavo2070@gmail.com>
2026-03-05 16:52:18 -0800

    add desktop testimonial

--- components/Testimonial.jsx
+++ components/Testimonial.jsx
@@ -4,10 +4,12 @@ export default function Testimonial({...rest}) {
     return (
         <div className="testimonial">
             <img src={rest.img}/>
-            <BiSolidQuoteAltLeft className="quote-icon"/>
-            <span className="text">{rest.text}</span>
-            <span className="name">{rest.name}</span>
-            <span className="position">{rest.position}</span>
+            <div className="testimonial-contents">
+                <BiSolidQuoteAltLeft className="quote-icon"/>
+                <span className="text">{rest.text}</span>
+                <span className="name">{rest.name}</span>
+                <span className="position">{rest.position}</span>
+            </div>
         </div>
     )
 }
