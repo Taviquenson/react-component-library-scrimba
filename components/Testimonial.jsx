6dc7d7b6edd58329ed466bc18062f0547e3c060f
Taviquenson <tavo2070@gmail.com>
2026-03-05 16:04:28 -0800

    add mobile testimonial

--- components/Testimonial.jsx
+++ components/Testimonial.jsx
@@ -1,10 +1,13 @@
 import { BiSolidQuoteAltLeft } from "react-icons/bi";
-// <BiSolidQuoteAltLeft />
 
 export default function Testimonial({...rest}) {
     return (
         <div className="testimonial">
-            
+            <img src={rest.img}/>
+            <BiSolidQuoteAltLeft className="quote-icon"/>
+            <span className="text">{rest.text}</span>
+            <span className="name">{rest.name}</span>
+            <span className="position">{rest.position}</span>
         </div>
     )
 }
